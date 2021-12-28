45ad76392179068766ecfd6136cbfe5590609129
Leisan Latypova <latley90@gmail.com>
2021-12-28 19:56:23 +0300

    Two views

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Leysan Latypova on 28.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
